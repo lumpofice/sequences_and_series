c2bcb1d68d1c6d5f8bbaa5d4fcaae6ca470783d3
Jonathan Parker <jparker007@gmail.com>
2022-05-24 18:29:15 -0400

    Adding user input and plotting visual to the geometric.py file

--- geometric.py
+++ geometric.py
@@ -1,11 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def series(a, r, epsilon):
     """This geometric series follows the definition
-    f(n) = sum_{i=0}^{\infty}(a*r**i), where a, r are constants and abs(r) < 1
-    Choose your epsilon small enough to ensure accuracy of the value of the 
+    seen in the legend, where a and r are constants and abs(r) < 1.
+    Choose your epsilon small enough to ensure accuracy of the sum of the 
     series"""
     results = []
+    powers = []
     power = 0
+    powers.append(power)
     prev_result = 0
+    results.append(np.nan)
     flag = True
     while flag:
         curr_result = a*r**(power)
@@ -14,4 +20,18 @@ def series(a, r, epsilon):
         results.append(curr_result)
         prev_result = curr_result
         power += 1
-    return sum(results)
+        powers.append(power)
+    return powers, results
+
+powers, terms = series(float(input('Choose a scalar: ')), 
+                       float(input('Choose a base: ')), 
+                       float(input('Choose an epsilon: ')))
+fig, ax = plt.subplots(figsize=(15, 10))
+ax.plot(powers, terms, label=r'$af(r)=\sum_{i=0}^{\infty}ar^i$')
+plt.xlabel('Integer Powers', fontsize=20)
+plt.ylabel('Sequence Value', fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+fig.suptitle('Geometric Series', fontsize=20)
+plt.legend(prop={'size': 20})
+plt.show()
