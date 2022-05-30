65537c5cd919ed553e6ea693eef2effd41e7173e
Jonathan Parker <jparker007@gmail.com>
2022-05-30 17:15:09 -0400

    Changing the scripts a bit to include more intuitive options for learning the concepts. Started this process with u_n_as_term_in_denom_num.py and .ipynb

--- sequences_of_functions/uniform_convergence/u_n_as_term_in_denom_num.py
+++ sequences_of_functions/uniform_convergence/u_n_as_term_in_denom_num.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s'
     ' - %(message)s')
 logging.getLogger('matplotlib.font_manager').disabled = True
 
+
 logging.debug('Start of program' + f'\n')
 
 
@@ -16,114 +17,315 @@ to evaluate True. Once the if conditional evaluates True, the function stores
 the number of natural numbers, n=K, for such an evaluation to be reached.
 This sequence of functions converges uniformly to f=x, on (-\infty, \infty)"""
     
-    # User input for x
-    flag = True
-    while flag:
-        # We take care of those scenarios when user input is the return key
-        x_input = input('Choose a real number x between 2/10 and 1,'
-            ' or press the return key to quit: ')
-        if not x_input:
+    
+    # We kick off the program
+    starter = True
+    while starter:
+        
+        
+        # We give users a choice of whether or not they wish to explore this
+        # function with a natural number or an epsilon
+        choose_k_or_e = input('Would you like to choose an arbitrarily'\
+                ' small number based on natural number k or positive epsilon?'\
+                ' Press k for natural number k, choose e for positive epsilon,'\
+                ' or press return to escape the program: \n')
+        
+        
+        if not choose_k_or_e:
+            starter = False
             return 'Okay. Goodbye'
         
-        # We take care of those scenarios when user input is non-numerical
-        try:
-            x = float(x_input)
-            flag = False
-        except ValueError:
-            logging.debug('Really must type in a numerical value.' + f'\n')
-            continue 
-    
-    # User input for epsilon
-    flag = True
-    while flag:
-        # We take care of those scenarios when user input is the return key
-        epsilon_input = input('Choose a small number epsilon, between 1/100' 
-            ' and 1/10, or press the return key to quit: ')
-        if not epsilon_input:
-            return 'Okay. Goodbye'
-        
-        # We take care of those scenarios when user input is non-numerical
-        try:
-            epsilon = float(epsilon_input)
-            flag = False
-        except ValueError:
-            logging.debug('Really must type in a numerical value.' + f'\n')
-            continue
-    
-    
-    # When user input for x is out of specified range
-    if x < 0.2 or x > 1:
-        logging.debug(f'Your value x={x} is not in the requested'
-            ' range' + f'\n')
-        return 'Goodbye'
-    
-    # When user input for epsilon is out of specified range
-    if epsilon < 1/100 or epsilon > 1/10:
-        logging.debug(f'Your value epsilon={epsilon} is not in the' 
-            ' requested range' + f'\n')
-        return 'Goodbye'
-    
-    # The statement below begins to assemble our figure
-    fig, ax = plt.subplots(figsize=(15, 10))
-    
-    # Vector u is what defines our restricted domain of each function
-    # with respect to x
-    u = np.linspace(-1000, 1000, 1000)
-    
-    # We initiate the index n with a value of 1 to simulate a
-    # mathematical sequence
-    n = 1
-    
-    # Vector v will serve as the range of our restricted domain defined by
-    # vector u
-    v = u + (1/n)
-    
-    # We plot this first vector pair u and v, as well as the 
-    # absolute value of the difference of f_n(x)-f(x), where f_n(x) is the
-    # function from our sequence at n=1 and f(x) is the function to which the
-    # sequence of functions uniformly converges.
-    ax.plot(u, v)
-    ax.scatter(x, (1/n), label=r'$| f_n(x) - f(x) |$')
-    
-    flag = True
-    while flag:
-        if abs(1/n) < epsilon:
             
-            # Completing the plot
-            ax.axvline(x=0, c='k')
-            ax.axhline(y=0, c='k')
-            ax.axhline(y=epsilon, c='m', label=r'$\epsilon$')
-            ax.plot(u, u, c='k', label=r'$f(x)=x$')
-            plt.xlabel('Restricted Domain with respect to x', fontsize=20)
-            plt.xticks(fontsize=10)
-            plt.ylabel('Range of Restricted Domain',
-                fontsize=20)
-            plt.yticks(fontsize=10)
-            plt.ylim(0, 1.5)
-            plt.xlim(-1 - epsilon, 1 + epsilon)
-            fig.suptitle(r'$f_{n}(x) = x + \dfrac{1}{n}$', fontsize=20)
-            plt.legend(prop={'size':20})
-            plt.show()
+        if choose_k_or_e == 'k':
+            starter = False
             
-            flag = False
-        else:
             
-            # We increase our index by 1
-            n += 1
+            # User input for x
+            flag = True
+            while flag:
+                
+                
+                x_input = input('Choose a real number x between 2/10 and 1,'
+                    ' or press the return key to quit: ')
+                
+                
+                # We take care of those scenarios when user input is the return
+                # key
+                if not x_input:
+                    return 'Okay. Goodbye'
+                
+                
+                # We take care of those scenarios when user input is
+                # non-numerical
+                try:
+                    x = float(x_input)
+                    # When user input for x is out of specified range
+                    if x < 0.2 or x > 1:
+                        logging.debug(f'Your value x={x} is not in the'\
+                            ' requested range' + f'\n')
+                        continue
+                    else:
+                        flag = False
+                except ValueError:
+                    logging.debug('Really must type in a numerical value.'\
+                        + f'\n')
+                    continue 
             
-            # We keep the same restricted domain with vector u, but our
-            # range with this new vector v will change, since n has changed
+            
+            # User input for k
+            flag = True
+            while flag:
+                
+                
+                k_input = input('Choose a large natural number k, between 1' 
+                    ' and 100, or press the return key to quit: ')
+                
+                
+                # We take care of those scenarios when user input is the return
+                # key
+                if not k_input:
+                    return 'Okay. Goodbye'
+                
+                
+                # We take care of those scenarios when user input is
+                # non-numerical
+                try:
+                    k = float(k_input)
+                    # When user input for k is out of specified range
+                    if k < 1 or k > 100:
+                        logging.debug(f'Your value k={k} is not in the' 
+                            ' requested range' + f'\n')
+                        continue
+                    else:
+                        flag = False
+                except ValueError:
+                    logging.debug('Really must type in a numerical value.'\
+                        + f'\n')
+                    continue
+            
+            
+            # The statement below begins to assemble our figure
+            fig, ax = plt.subplots(figsize=(15, 10))
+            
+            
+            # Vector u is what defines our restricted domain of each function
+            # with respect to x
+            u = np.linspace(-1000, 1000, 1000)
+            
+            
+            # We initiate the index n with a value of 1 to simulate a
+            # mathematical sequence
+            n = 1
+            
+            
+            # Vector v will serve as the range of our restricted domain defined
+            # by vector u
             v = u + (1/n)
             
-            # We plot the next vector pair u and v in the same plot as the
-            # preceding vector pairs, as well as the absolute value of the
-            # difference of f_n(x)-f(x), where f_n(x) is the
-            # function from our sequence at n=k and f(x) is the function to
+            
+            # We plot this first vector pair u and v, as well as the 
+            # absolute value of the difference of f_n(x)-f(x), where f_n(x) is 
+            # the function from our sequence at n=1 and f(x) is the function to 
             # which the sequence of functions uniformly converges.
             ax.plot(u, v)
-            ax.scatter(x, (1/n))
+            ax.scatter(x, (1/n), label=r'$| f_n(x) - f(x) |$')
             
-    return f'\nThe number of functions on the graph: {n}'
+            
+            flag = True
+            while flag:
+                
+                
+                if abs(1/n) < 1/k:
+                    # Completing the plot
+                    ax.axvline(x=0, c='k')
+                    ax.axhline(y=0, c='k')
+                    ax.axhline(y=1/k, c='m', label=r'$\epsilon$')
+                    ax.plot(u, u, c='k', label=r'$f(x)=x$')
+                    plt.xlabel('Restricted Domain with respect to x',\
+                        fontsize=20)
+                    plt.xticks(fontsize=10)
+                    plt.ylabel('Range of Restricted Domain',\
+                        fontsize=20)
+                    plt.yticks(fontsize=10)
+                    plt.ylim(0, 1.5)
+                    plt.xlim(-1 - (1/k), 1 + (1/k))
+                    fig.suptitle(r'$f_{n}(x) = x + \dfrac{1}{n}$', fontsize=20)
+                    plt.legend(prop={'size':20})
+                    plt.show()
+                    
+                    
+                    flag = False
+                    
+                    
+                else:
+                    # We increase our index by 1
+                    n += 1
+                    
+                    
+                    # We keep the same restricted domain with vector u, but our
+                    # range with this new vector v will change, since n has
+                    # changed
+                    v = u + (1/n)
+                    
+                    
+                    # We plot the next vector pair u and v in the same plot as 
+                    # the preceding vector pairs, as well as the absolute value 
+                    # of the difference of f_n(x)-f(x), where f_n(x) is the
+                    # function from our sequence at n=k and f(x) is the function 
+                    # to which the sequence of functions uniformly converges.
+                    ax.plot(u, v)
+                    ax.scatter(x, (1/n))
+                    
+                    
+            return f'\nThe number of functions on the graph: {n}'
+        
+        
+        if choose_k_or_e == 'e':
+            starter = False
+            
+            
+            # User input for x
+            flag = True
+            while flag:
+                
+                
+                x_input = input('Choose a real number x between 2/10 and 1,'
+                    ' or press the return key to quit: ')
+                
+                
+                # We take care of those scenarios when user input is the return
+                # key
+                if not x_input:
+                    return 'Okay. Goodbye'
+                
+                
+                # We take care of those scenarios when user input is
+                # non-numerical
+                try:
+                    x = float(x_input)
+                    # When user input for x is out of specified range
+                    if x < 0.2 or x > 1:
+                        logging.debug(f'Your value x={x} is not in the'\
+                            ' requested range' + f'\n')
+                        continue
+                    else:
+                        flag = False
+                except ValueError:
+                    logging.debug('Really must type in a numerical value.'\
+                        + f'\n')
+                    continue 
+            
+            
+            # User input for epsilon
+            flag = True
+            while flag:
+                
+                
+                epsilon_input = input('Choose a small number epsilon, between'\
+                    ' 1/100 and 1/10, or press the return key to quit: ')
+                
+                
+                # We take care of those scenarios when user input is the return
+                # key
+                if not epsilon_input:
+                    return 'Okay. Goodbye'
+                
+                
+                # We take care of those scenarios when user input is
+                # non-numerical
+                try:
+                    epsilon = float(epsilon_input)
+                    # When user input for epsilon is out of specified range
+                    if epsilon < 1/100 or epsilon > 1/10:
+                        logging.debug(f'Your value epsilon={epsilon} is not'\
+                            ' in the requested range' + f'\n')
+                        continue
+                    else:
+                        flag = False
+                except ValueError:
+                    logging.debug('Really must type in a numerical value.'\
+                        + f'\n')
+                    continue
+            
+            
+            # The statement below begins to assemble our figure
+            fig, ax = plt.subplots(figsize=(15, 10))
+            
+            
+            # Vector u is what defines our restricted domain of each function
+            # with respect to x
+            u = np.linspace(-1000, 1000, 1000)
+            
+            
+            # We initiate the index n with a value of 1 to simulate a
+            # mathematical sequence
+            n = 1
+            
+            
+            # Vector v will serve as the range of our restricted domain defined 
+            # by vector u
+            v = u + (1/n)
+            
+            
+            # We plot this first vector pair u and v, as well as the 
+            # absolute value of the difference of f_n(x)-f(x), where f_n(x) is
+            # the function from our sequence at n=1 and f(x) is the function to 
+            # which the sequence of functions uniformly converges.
+            ax.plot(u, v)
+            ax.scatter(x, (1/n), label=r'$| f_n(x) - f(x) |$')
+            
+            
+            flag = True
+            while flag:
+                
+                
+                if abs(1/n) < epsilon:
+                    # Completing the plot
+                    ax.axvline(x=0, c='k')
+                    ax.axhline(y=0, c='k')
+                    ax.axhline(y=epsilon, c='m', label=r'$\epsilon$')
+                    ax.plot(u, u, c='k', label=r'$f(x)=x$')
+                    plt.xlabel('Restricted Domain with respect to x',\
+                        fontsize=20)
+                    plt.xticks(fontsize=10)
+                    plt.ylabel('Range of Restricted Domain',\
+                        fontsize=20)
+                    plt.yticks(fontsize=10)
+                    plt.ylim(0, 1.5)
+                    plt.xlim(-1 - epsilon, 1 + epsilon)
+                    fig.suptitle(r'$f_{n}(x) = x + \dfrac{1}{n}$', fontsize=20)
+                    plt.legend(prop={'size':20})
+                    plt.show()
+                    
+                    
+                    flag = False
+                    
+                    
+                else:
+                    # We increase our index by 1
+                    n += 1
+                    
+                    
+                    # We keep the same restricted domain with vector u, but our
+                    # range with this new vector v will change, since n has
+                    # changed
+                    v = u + (1/n)
+                    
+                    
+                    # We plot the next vector pair u and v in the same plot as 
+                    # the preceding vector pairs, as well as the absolute value 
+                    # of the difference of f_n(x)-f(x), where f_n(x) is the
+                    # function from our sequence at n=k and f(x) is the function
+                    # to which the sequence of functions uniformly converges.
+                    ax.plot(u, v)
+                    ax.scatter(x, (1/n))
+                    
+                    
+            return f'\nThe number of functions on the graph: {n}'
+        
+        
+        else:
+            continue
             
 
 print(x_plus_one_over_n())
