53b8d5a6c959f2bf88a1f82c0d2e2d086d6eafb8
Jonathan Parker <jparker007@gmail.com>
2022-07-01 10:49:50 -0400

    trying again to get this image to render. this time to the tikzpicture environment contains some information. before, it did not. i had to remove the x and y labels. will need to find a way to avoid this in the future. for now, i just want to make sure i get the image into overleaf

--- limits_of_functions/sine_nondomain_limit.py
+++ limits_of_functions/sine_nondomain_limit.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 
 x = np.linspace(0,0.49,5000)
@@ -9,14 +8,12 @@ u = np.linspace(0.51, 1, 5000)
 v = np.sin(u)
 
 
-fig, ax = plt.subplots(figsize=(15,10))
+fig, ax = plt.subplots()
 ax.plot(x, y, c='b')
 ax.plot(u, v, c='b')
 circle = plt.Circle((0.5, np.sin(0.5)), 0.01, fill=False)
 ax.set_aspect(1)
 ax.add_artist(circle)
-plt.xlabel('Domain', fontsize=20)
-plt.ylabel('Range', fontsize=20)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.xlim(0, 1)
@@ -25,9 +22,6 @@ fig.suptitle('$f(x)=\sin(x)$, ' + ' ' + ' $x \in [0,0.5)\cup(0.5,1]$',\
     fontsize=20)
 
 
-plt.close()
-mpl.rcParams.update(mpl.rcParamsDefault)
-
-
 import tikzplotlib
-tikzplotlib.save('sine_nondomain_limit.tex')
+tikzplotlib.save('sine_nondomain_limit.tex', flavor='context')
+
