4f8722d0507443cbd8f5623d3d8d0a3dd2aa4184
lumpofice <93841699+lumpofice@users.noreply.github.com>
2022-07-17 15:25:25 -0400

    adding to the derivative_of_functions.tex file

--- derivatives_of_functions/python_scripts/sin_0_pi_secants_tangent_0_pi.py
+++ derivatives_of_functions/python_scripts/sin_0_pi_secants_tangent_0_pi.py
@@ -1,33 +1,95 @@
 import numpy as np
 
-f = lambda u: np.sin(u)
-s1 = lambda w1:\
-    ((6*np.sqrt(3)-6*np.sqrt(2))/(5*np.pi))*w1 +\
-    ((8*np.sqrt(2)-3*np.sqrt(3))/10)
-s2 = lambda w2:\
-    ((4-(2*np.sqrt(2)))/(np.pi))*w2 + (np.sqrt(2)-1)
-t = lambda v: (np.sqrt(2)/2)*v + (np.sqrt(2)/2)*((4-np.pi)/4)
 
-f_s1_t_xs = [
+#-------------------------------------------------------------------------------
+# The sine function
+# 1) function
+g = lambda v: np.sin(v)
+# 2) domain
+g_xs = [
     np.arange(0, np.pi, 0.01)
     ]
+# 3) generated data table
+with open('sin_0_pi.table', 'w') as file:
+    for v in g_xs[0]:
+        if g(v) >= 0 and g(v) <= 1:
+            file.write('{:f} {:f}\n'.format(v, g(v)))
 
-for i, j in enumerate(f_s1_t_xs):
-    with open('sin_0_pi.table', 'w') as file:
-        for u in j:
-            if f(u) >= 0 and f(u) <= 1:
+
+#-------------------------------------------------------------------------------
+# A set of secant lines
+for n in range(1, 5):
+    # 1) slope
+    m = (np.sin((np.pi/4) + (1/n)) - np.sin(np.pi/4))/(1/n)
+    # 2) intercept
+    b = np.sin(np.pi/4) - m*(np.pi/4)
+    # 3) function
+    f = lambda u: m*u + b
+    # 4) domain 
+    f_xs = [
+        np.arange(0, np.pi, 0.01)
+        ]
+    # 5) generated data table
+    with open('sin_0_pi_line_{}.table'.format(n), 'w') as file:
+        for u in f_xs[0]:
+            if f(u) >= 0 and f(u) <= 3:
                 file.write('{:f} {:f}\n'.format(u, f(u)))
-    with open('secant1_0_pi.table', 'w') as file:
-        for w1 in j:
-            if s1(w1) >= 0 and s1(w1) <=1:
-                file.write('{:f} {:f}\n'.format(w1, s1(w1)))
-    with open('secant2_0_pi.table', 'w') as file:
-        for w2 in j:
-            if s2(w2) >= 0 and s2(w2) <= 2:
-                file.write('{:f} {:f}\n'.format(w2, s2(w2)))
-    with open('tangent_0_pi.table', 'w') as file:
-        for v in j:
-            if t(v) >= 0 and\
-                t(v) <= (np.sqrt(2)/2)*(np.pi) + (np.sqrt(2)/2)*((4-np.pi)/4):
-                file.write('{:f} {:f}\n'.format(v, t(v)))
-                
+        
+
+#-------------------------------------------------------------------------------
+# Another set of secant lines
+
+
+# First line
+# 1) slope
+m = (np.sin(2*np.pi/3) - np.sin(np.pi/4))/(5*np.pi/12)
+# 2) intercept
+b = np.sin(np.pi/4) - m*(np.pi/4) 
+# 3) function
+s = lambda x: m*x + b
+# 4) domain
+s_xs = [
+    np.arange(0, np.pi, 0.01)
+    ]
+# 5) generated data table
+with open('sin_0_pi_line_5.table', 'w') as file:
+    for x in s_xs[0]:
+        if s(x) >= 0 and s(x) <= 3:
+            file.write('{:f} {:f}\n'.format(x, s(x)))
+            
+            
+# Second line
+# 1) slope
+m = (np.sin(np.pi/2) - np.sin(np.pi/4))/(np.pi/4)
+# 2) intercept
+b = np.sin(np.pi/4) - m*(np.pi/4) 
+# 3) function
+w = lambda y: m*y + b
+# 4) domain
+w_xs = [
+    np.arange(0, np.pi, 0.01)
+    ]
+# 5) generated data table
+with open('sin_0_pi_line_6.table', 'w') as file:
+    for y in w_xs[0]:
+        if w(y) >= 0 and w(y) <= 3:
+            file.write('{:f} {:f}\n'.format(y, w(y)))
+            
+
+#-------------------------------------------------------------------------------
+# The tangent line
+# 1) slope
+m = np.sin(np.pi/4)
+# 2) intercept
+b = np.sin(np.pi/4) - m*(np.pi/4) 
+# 3) function
+t = lambda z: m*z + b
+# 4) domain
+t_xs = [
+    np.arange(0, np.pi, 0.01)
+    ]
+# 5) generated data table
+with open('sin_0_pi_tangent.table', 'w') as file:
+    for z in t_xs[0]:
+        if t(z) >= 0 and t(z) <= 3:
+            file.write('{:f} {:f}\n'.format(z, t(z)))
